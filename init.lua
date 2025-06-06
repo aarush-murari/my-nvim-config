c129f5756fcc9158db85f1113a9186afbfaa6b2f
aarush-murari <aarush.murari@gmail.com>
2025-06-06 00:16:45 -0700

    added neogit and tab is now 4 spaces

--- init.lua
+++ init.lua
@@ -4,5 +4,12 @@ vim.cmd([[set shellpipe=>%s\ 2>&1]])
 
 vim.opt.number = true
 
+
+vim.o.tabstop = 8
+vim.o.smarttab = true
+vim.o.expandtab = true
+vim.o.shiftwidth = 4
+vim.o.softtabstop = 4
+
 require("aarushmurari.core")
 require("aarushmurari.lazy")
