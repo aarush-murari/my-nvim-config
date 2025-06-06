7cae51adfdb0fd48d8f8e98c4e3e27ba2a6c0932
aarush-murari <aarush.murari@gmail.com>
2025-06-06 00:52:04 -0700

    added java lsp I guess

--- lua/aarushmurari/plugins/mason.lua
+++ lua/aarushmurari/plugins/mason.lua
@@ -2,7 +2,7 @@
 return {
     "mason-org/mason-lspconfig.nvim",
     opts = {
-	ensure_installed = { "clangd", "lua_ls" },
+	ensure_installed = { "clangd", "lua_ls", "jdtls" },
     },
     dependencies = {
         { "mason-org/mason.nvim", opts = {} },
