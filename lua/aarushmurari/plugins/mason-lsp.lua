bb1f278a4caf6a4891201d7ccddb86bbcfcfd877
aarush.murari <aarush.murari@gmail.com>
2025-06-20 08:14:49 -0700

    renamed mason to mason-lsp for my clarity

--- lua/aarushmurari/plugins/mason-lsp.lua
+++ lua/aarushmurari/plugins/mason-lsp.lua
@@ -2,10 +2,12 @@
 return {
     "mason-org/mason-lspconfig.nvim",
     opts = {
-	ensure_installed = { "clangd", "lua_ls", "jdtls", "neocmake", "pyright", "cssmodules_ls", "denols", "html", "jinja_lsp", "black"},
+	ensure_installed = { "clangd", "lua_ls", "jdtls", "neocmake", "pyright", "cssmodules_ls", "denols", "html", "jinja_lsp"},
     },
     dependencies = {
-        { "mason-org/mason.nvim", opts = {} },
+        {
+            "mason-org/mason.nvim", opts={},
+        },
         "neovim/nvim-lspconfig",
         "hrsh7th/cmp-nvim-lsp",
     },
