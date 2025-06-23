b708a12a279e0a87852bb84c28d4f7de966baaa4
aarush.murari <aarush.murari@gmail.com>
2025-06-23 02:29:50 -0700

    Fixed keybinding issues with neogen

--- lua/aarushmurari/plugins/neogen.lua
+++ lua/aarushmurari/plugins/neogen.lua
@@ -1,8 +1,6 @@
-return { 
+local opts = { noremap = true, silent = true }
+vim.api.nvim_set_keymap("n", "<Leader>nd", ":lua require('neogen').generate()<CR>", opts)
+return {
     "danymat/neogen", -- for class annotations and shit
     config = true,
-    function()
-        local opts = { noremap = true, silent = true },
-        vim.api.nvim_set_keymap("n", "<Leader>nd", function() require('neogen').generate() end, opts)
-    end,
 }
