0472b3e7a14085f6a4e869f48edd4ec77423c562
aarush.murari <aarush.murari@gmail.com>
2025-06-27 06:35:56 -0700

    Added python debugger support via nvim-dap

---
+++ lua/aarushmurari/plugins/debugging.lua
@@ -0,0 +1,36 @@
+return {
+    "mfussenegger/nvim-dap",
+    dependencies = {
+        "nvim-neotest/nvim-nio",
+        "rcarriga/nvim-dap-ui",
+        "mfussenegger/nvim-dap-python"
+    },
+    config = function()
+        local dap = require("dap")
+        local dapui = require("dapui")
+
+        dapui.setup()
+
+        if (vim.loop.os_uname().sysname == "Windows_NT") then
+            require("dap-python").setup("py") --- For windows
+        else 
+            require("dap-python").setup("python3") --- For unix/linux
+        end
+
+        dap.listeners.before.attach.dapui_config = function()
+          dapui.open()
+        end
+        dap.listeners.before.launch.dapui_config = function()
+          dapui.open()
+        end
+        dap.listeners.before.event_terminated.dapui_config = function()
+          dapui.close()
+        end
+        dap.listeners.before.event_exited.dapui_config = function()
+          dapui.close()
+        end
+        vim.keymap.set('n', '<Leader>dt', dap.toggle_breakpoint, {}) 
+        vim.keymap.set('n', '<Leader>dc', dap.continue, {})
+
+    end,
+}
