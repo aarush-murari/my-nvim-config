15ce95799af10105b29a29e21981d69c747127bc
aarush.murari <aarush.murari@gmail.com>
2025-06-30 01:56:50 -0700

    Fixed mapping issues. Now I got inline terminal like vscode but in nvim simply for ease of transition

--- lua/aarushmurari/plugins/vscode_cfg.lua
+++ lua/aarushmurari/plugins/vscode_cfg.lua
@@ -1,6 +1,5 @@
+local keymap = vim.keymap -- for conciseness
 if vim.g.vscode then
-    local keymap = vim.keymap -- for conciseness
-
     keymap.set("n", "<Leader>vrf", function() local vscode = require('vscode') vscode.call('editor.action.refactor') end, { noremap = true, silent = true }) -- refactor shit I guess
 
     keymap.set("n", "<leader>ee", function() local vscode = require('vscode') vscode.call('workbench.action.toggleSidebarVisibility') end, { noremap = true, silent = true, desc = "toggle dat sidebar"}) --toggle side bar so I don;t accidentally learn a different keybinding in regular vim
@@ -9,6 +8,8 @@ if vim.g.vscode then
     keymap.set("n", "<leader>ec", function() local vscode = require('vscode') vscode.call('workbench.files.action.collapseExplorerFolders') end, { noremap = true, silent = true, desc = "Collapse file explorer" }) -- collapse file explorer
     keymap.set("n", "<leader>er", function() local vscode = require('vscode') vscode.call('workbench.files.action.refreshFilesExplorer') end, { noremap = true, silent = true, desc = "Refresh file explorer" }) -- refresh file explorer
     keymap.set("n", "<leader>pv", "<cmd>Ex<CR>", { noremap = true, silent = true, desc = "Toggle file explorer" })
+else
+    keymap.set("n", "<C-F1>", "<cmd>split<CR><C-w>j<cmd>term<CR>", { noremap = true, silent = true, desc = "Terminal like vscode"}) -- yes bitch, this is indeed a terminal binding bitch
 end
 
 return {}
